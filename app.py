4aafb954d200a64ecc4491686f111dc6c5330252
JorgeGR-16 <jgr@azc.uam.mx>
2025-06-23 15:07:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.markdown("""
         }
         .block-container {
             background-color: #FFFFFF;
-            border-radius: 600px;
+            border-radius: 15px;
             padding: 6rem;
             box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.05);
         }
